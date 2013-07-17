150a1ad9c7c864959a2483202952e1d415a29c9d
Andy Brown <andy@andybrown.me.uk>
2013-07-17 20:37:20 +0100

    include license header in examples

--- examples/arduinoIDE/ILI9325/Weather/Weather.ino
+++ examples/arduinoIDE/ILI9325/Weather/Weather.ino
@@ -1,20 +1,27 @@
 /*
- * XMEM LCD Library for the Arduino
- * Copyright (c) 2012 Andy Brown. All rights reserved.
- * This is open source software. Please see website for terms and conditions:
- *
- * http://andybrown.me.uk
- *
- * This notice may not be removed or altered from any source distribution.
- *
- * Please respect the rights of the original authors of the weather images
- * used ILI9325in this demo code:
- *
- * http://en.wikipedia.org/wiki/File:Nuvola_weather_snow.svg
- * http://commons.wikimedia.org/wiki/File:Crystal_Project_kweather.png
- * http://commons.wikimedia.org/wiki/File:Weather-drizzle.svg
- * http://commons.wikimedia.org/wiki/File:Sun.svg
- */
+  XMEM LCD Library for the Arduino
+
+  Copyright 2012,2013 Andrew Brown
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This notice may not be removed or altered from any source distribution.
+
+  http://en.wikipedia.org/wiki/File:Nuvola_weather_snow.svg
+  http://commons.wikimedia.org/wiki/File:Crystal_Project_kweather.png
+  http://commons.wikimedia.org/wiki/File:Weather-drizzle.svg
+  http://commons.wikimedia.org/wiki/File:Sun.svg
+*/
 
 #include "Generic16BitILI9325.h"
 
