79ed18dd074fa2086cd7e9deca79dab45616b21a
Andy Brown <andy@andybrown.me.uk>
2013-07-17 13:27:54 +0100

    apache v2 license migration

--- lib/gl/Point.h
+++ lib/gl/Point.h
@@ -1,12 +1,22 @@
 /*
- * XMEM LCD Library for the Arduino
- * Copyright (c) 2012,2013 Andy Brown. All rights reserved.
- * This is open source software. Please see website for terms and conditions:
- *
- * http://andybrown.me.uk
- *
- * This notice may not be removed or altered from any source distribution.
- */
+  XMEM LCD Library for the Arduino
+
+  Copyright 2012,2013 Andrew Brown
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This notice may not be removed or altered from any source distribution.
+*/
 
 /**
  * @file Point.h
